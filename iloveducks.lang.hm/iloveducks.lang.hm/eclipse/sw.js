b982e4792bd28438afaa25d3582bcfa49ab64da0
bt6wmsn7nh-lang <bt6wmsn7nh@privaterelay.appleid.com>
2026-03-13 06:49:05 -0600

    Add files via upload

---
+++ iloveducks.lang.hm/iloveducks.lang.hm/eclipse/sw.js
@@ -0,0 +1,21 @@
+importScripts("/eclipse/uv/uv.bundle.js")
+importScripts("/eclipse/uv/uv.config.js")
+//importScripts(__uv$config.sw || "/eclipse/uv/uv.sw.js")
+importScripts("/eclipse/uv/uv.sw.js")
+
+const uv = new UVServiceWorker()
+
+
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        (async function() {
+
+            if (event.request.url.startsWith(location.origin + "/eclipse/")) {
+                return await uv.fetch(event)
+            }
+
+            return await fetch(event.request)
+        })()
+    )
+})
